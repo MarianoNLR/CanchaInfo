e6abf05d4ae4b4fc0a7020ac8042f53700da56ac
MarianoNLR <marianonehuenlr@gmail.com>
2023-05-23 04:59:22 -0300

    fix

--- CanchaF5/CanchaF5/settings.py
+++ CanchaF5/CanchaF5/settings.py
@@ -85,14 +85,10 @@ WSGI_APPLICATION = 'CanchaF5.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-            default='sqlite:///PATH',
-            conn_max_age=600
-        )
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        
-    
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
